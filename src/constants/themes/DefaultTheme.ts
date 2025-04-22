0b7231cde73d36f2d59dbb055e1b52ff59ff2350
Lilian Marzet <lilian.marzet123@gmail.com>
2025-04-22 17:04:11 +0200

    [FEAT] Ajout du contexte de gestion des themes

---
+++ src/constants/themes/DefaultTheme.ts
@@ -0,0 +1,8 @@
+export const DefaultTheme = {
+    primary: '#3c3f41',
+    secondary: '#313336',
+    tertiary: '#0d293e',
+    background: '#3c3f41',
+    text: '#FFFFFF',
+    outline: '#505051',
+}
--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { ThemeProvider } from '@hooks/ThemeContext';
 
 const root = ReactDOM.createRoot(
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     document.getElementById('root')!
 )
 
 root.render(
     <React.StrictMode>
-        <App />
+        <ThemeProvider>
+            <App />
+        </ThemeProvider>
     </React.StrictMode>
 );
