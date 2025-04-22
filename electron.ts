6990f16fe68a604192bfc2b4e2782ff9ae868f3b
Lilian Marzet <lilian.marzet123@gmail.com>
2025-04-22 16:37:37 +0200

    [FEAT] Ajout d'electron et generation d'une desktop app de dev après build

---
+++ electron.ts
@@ -0,0 +1,42 @@
+import { app, BrowserWindow } from 'electron';
+import * as path from 'path';
+import * as url from 'url';
+
+let mainWindow: BrowserWindow | null = null;
+
+function createWindow(): void {
+    mainWindow = new BrowserWindow({
+        fullscreen: true,
+        webPreferences: {
+            contextIsolation: true,
+        },
+    });
+
+    const startUrl = url.format({
+        pathname: path.join(__dirname, '../build/index.html'),
+        protocol: 'file:',
+        slashes: true,
+    });
+
+    mainWindow.loadURL(startUrl);
+
+    mainWindow.webContents.openDevTools();
+
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+}
+
+app.whenReady().then(() => {
+    createWindow();
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+});
