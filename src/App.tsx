0eddc1a6d890e2678c03b1650ad26580e0a8eb2c
Lilian Marzet <lilian.marzet123@gmail.com>
2025-04-22 15:24:36 +0200

    [FEAT] Ajout de Craco et Typescript

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React, {type ReactNode} from "react";
+import './App.css';
+
+const App = (): ReactNode => (
+    <div className="App">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+    </div>
+);
+
+export default App;
