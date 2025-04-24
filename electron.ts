2e32c95d20640ef256ae5e95a97faac4233e6413
Lilian Marzet <lilian.marzet123@gmail.com>
2025-04-25 01:01:16 +0200

    [FEAT] Configuration de l'environnement de dev de electron

--- electron.ts
+++ electron.ts
@@ -6,21 +6,27 @@ let mainWindow: BrowserWindow | null = null
 
 function createWindow(): void{
     mainWindow = new BrowserWindow({
-        fullscreen: true,
+        fullscreen: false,
+        width: 1280,
+        height: 800,
         webPreferences: {
             contextIsolation: true
         }
     })
 
-    const startUrl = url.format({
-        pathname: path.join(__dirname, '../build/index.html'),
-        protocol: 'file:',
-        slashes: true
-    })
+    const isDev = !app.isPackaged
+
+    const startUrl = isDev
+        ? 'http://localhost:3000'
+        : url.format({
+            pathname: path.join(__dirname, '../build/index.html'),
+            protocol: 'file:',
+            slashes: true
+        })
 
     mainWindow.loadURL(startUrl)
 
-    mainWindow.webContents.openDevTools()
+    // mainWindow.webContents.openDevTools()
 
     mainWindow.on('closed', () => {
         mainWindow = null
